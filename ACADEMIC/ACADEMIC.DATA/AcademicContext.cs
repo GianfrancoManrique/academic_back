94e7fba4d34fc91d97eef32d5b2b0aacedbbd693
gmanriquev <gmanrique.422@gmail.com>
2021-01-23 11:05:08 -0500

    Data tier.

---
+++ ACADEMIC/ACADEMIC.DATA/AcademicContext.cs
@@ -0,0 +1,20 @@
+﻿using ACADEMIC.DOMAIN;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ACADEMIC.DATA
+{
+    public class AcademicContext:DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Inscription> Inscriptions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"
+            Server=tcp:devmasterperu.database.windows.net,1433;Initial Catalog=AcademicDB;Persist Security Info=False;User ID=student;Password=Qazw3579@_;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
